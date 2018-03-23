ffe31a58501afd48db6cf30bc962958698871121
5755 <5755@ait-sydney.net.au>
2018-03-23 12:22:55 +1100

    first commit

---
+++ ts/todo.ts
@@ -0,0 +1,11 @@
+class Task{
+  id: number;
+  name: string;
+  status: boolean;
+  constructor(){
+   this.name = name;
+  this.id = new Date().getTime();
+    this.status = false;
+    return this;
+  }
+}
